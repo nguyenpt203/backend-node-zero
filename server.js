09c1edc56007dc072aab29bf287a2dd7f0d247b7
nguyenpt203 <nguyenpt203@gmail.com>
2024-10-05 17:15:35 +0700

    video 17

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/abc', (req, res) => {
+    res.send('ABC')
+  })
+
+  app.get('/nguyen', (req, res) => {
+    res.send('<h1>nguyen phung thai</h1>')
+  }) 
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
